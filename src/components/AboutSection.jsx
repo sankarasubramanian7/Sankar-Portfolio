5c312b7198104313e8f171a92f96556e4374035b
sankarasubramanian7 <gsankarasubramaniann@gmail.com>
2025-05-05 20:18:34 +0530

    Final commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Sankar Resume.pdf"
+                href="/Sankar7 Resume.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 >
